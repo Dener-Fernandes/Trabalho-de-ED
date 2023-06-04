42a53fe619ab53d81ae7f4203713ee7d32aea09e
Dener Fernandes de Oliveira <dener.fernandes.oliveira@gmail.com>
2023-06-04 12:44:04 -0400

    Create estruturas.h

---
+++ estruturas.h
@@ -0,0 +1,39 @@
+typedef struct municipio {
+  int codigoIBGE;
+  char nome[50];
+  int codigoUf;
+  int capital;
+  float latitude;
+  float longitude;
+  int siafi;
+  int ddd;
+  char fusoHorario[50];
+  char coordenada[15];
+} municipio;
+
+typedef struct fastFood {
+  char address[50];
+  char category[50];
+  char city[50];
+  char country[50];
+  char name[50];
+  int postalCode;
+  char province[50];
+  char website[100];
+  float latitude;
+  float longitude;
+  char nome[50];
+  char coordenada[15];
+} fastFood;
+
+typedef struct no {
+  void *data;
+  struct no *esquerda;
+  struct no *direita;
+  struct no *pai;
+} no;
+
+typedef struct arvore {
+  no *raiz;
+  int (*comparador)(const void *x, const void *y);
+} arvore;
